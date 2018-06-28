df54d312ed77219378e4cd1e28d140f67c89d3fd
Lasha Dolidze <lashad@mac.com>
2018-06-28 19:47:15 +0400

    Round 1

--- index.js
+++ index.js
@@ -4,19 +4,178 @@
 
 var request = require('request');
 
-// Init the module
-module.exports = (function () {
 
-  var defTimeout = 10000,
-      defLang    = 'ka',
-      host       = 'https://admin.aliengreen.ge/';
+var mySingleton = (function () {
 
-  var get = function get() {
+    // Instance stores a reference to the Singleton
+    var instance;
 
-      return defTimeout++;
+    var defTimeout   = 10000;
+        xdefLang     = 'en';
+        host         = 'https://admin.aliengreen.ge';
+        access_token = undefined;
+
+  function init() {
+
+    // Singleton
+
+        /* ------------------------------------------------------------ */
+
+        // Private methods and variables
+        async function callApi(uri, param, key) {
+
+         var options = {
+              url: host + uri,
+              method: 'POST',
+              json: true, 
+              body: param
+            };
+
+          if(key !== undefined) {
+            options.headers = {
+                'Authorization': 'Bearer ' + key
+            };
+          }
+
+          return await new Promise((resolve, reject) => {
+            request(options, function (error, response, body) {
+              if(error) {
+                reject({code: error.errno, error: error.message});
+              } else { 
+                  if(response.statusCode != 200) {
+                    reject({code: response.statusCode, error: statusCodeToMessage(response.statusCode)});
+                  } else {
+                    resolve(body);
+                  }
+              }
+             });
+          });
+        }
+
+        /* ------------------------------------------------------------ */
+
+        function statusCodeToMessage(statusCode) {
+
+            if(statusCode == 401) {
+                return "Unauthorized request, invalid email/password or invalid access token";
+            } else if(statusCode == 408) { 
+                return "Device is offline";
+            } else if(statusCode == 404) {
+                return "Device not found, invalid EUI64 serial number";
+            } else if(statusCode == 418) {
+                return "The remote server is a teapot (" + statusCode + ")";
+            } else {
+                return "Unknown status code " + statusCode;
+            }
+        }
+
+        /* ------------------------------------------------------------ */
+
+    return {
+
+            // Public methods and variables
+            login: async (email, password) => {
+
+                var param = {
+                  email: email,
+                  password: password
+                };
+
+                try {
+                    let result = await callApi('/auth/login', param);
+                    access_token = result.access_token;
+                    return {access_token: result.access_token};
+                } catch (err) {
+                   return err;
+                }
+            },
+          
+            getRegister: async(eui64, reg) => {
+
+                var param = {
+                  euid_64: eui64,
+                  type: 'get_register',
+                  register_name: reg
+                };
+
+                try {
+                    let result = await callApi('/api/registers', param, access_token);
+                    return {result: result.result.records};
+                } catch (err) {
+                   return err;
+                }
+
+            },
+
+            putRegister: async(eui64, reg, value, value_len, type) => {
+
+                var param = {
+                  euid_64: eui64,
+                  type: 'put_register',
+                  register_name: reg,
+                  register_value: value,
+                  register_value_len: value_len,
+                  register_value_type: type
+                };
+
+                try {
+                    let result = await callApi('/api/registers', param, access_token);
+                    return {result: result.result};
+                } catch (err) {
+                   return err;
+                }
+            },
+
+            getStatus: async(eui64) => {
+
+                var param = {
+                  euid_64: eui64,
+                  type: 'get_status',
+                };
+
+                try {
+                    let result = await callApi('/api/registers', param, access_token);
+                    return {result: result.result};
+                } catch (err) {
+                   return err;
+                }
+
+            },
+
+            restartDevice: async(eui64) => {
+
+                try {
+                    let result = await instance.putRegister(eui64, 0x0136, 1, 1, 'uint');
+                    return result;
+                } catch (err) {
+                   return err;
+                }
+            }
+
+
+    };
+
   };
 
   return {
-    get: get,
+
+    // Get the Singleton instance if one exists
+    // or create one if it doesn't
+    getInstance: function () {
+
+      if ( !instance ) {
+        instance = init();
+      }
+
+      return instance;
+    }
+
   };
+
 })();
+
+module.exports = mySingleton.getInstance();
+
+
+
+
--- test/test-all.js
+++ test/test-all.js
@@ -3,54 +3,25 @@
 /* global describe: false, it: false */
 'use strict';
 
-var gwp = require('../'),
+var udsc = require('../'),
     expect  = require('chai').expect;
 
 // Tests
 
-describe('gwp-emergencies', function() {
+describe('uds-client', async function() {
 
   // find
-  describe('get()', function() {
-    it('should get a emergency info with given date 2018-05-08', function(done) {
-      gwp.get({lang: 'ka', date: '2018-05-08', street_name: 'პოლიტკოვსკაია'}, function(err, result) {
-        if(err) return done(err);
+  describe('login()', async function() {
+    it('should get a access token if we login successfully', async function(done) {
+        
+        // let result = udsc.login();
+        // expect(result).to.be.a('object');
+        // expect(result).to.have.property('access_token');
+        // done();
 
-        expect(err).to.be.equal(null);
-        expect(result).to.be.a('array');
-        expect(result).to.have.deep.property('[0].address', 'პოლიტკოვსკაიას ქ.');
-
-        done();
-      });
     });
 
-    it('should fail to get a information (missing options)', function(done) {
-      gwp.get(null, function(err, result) {
-        if(!err) return done('No error!');
-
-        expect(result).to.be.equal(undefined);
-        done();
-      });
-    });
-
-    it('should not return any information (bad date)', function(done) {
-      gwp.get({date: '.'}, function(err, result) {
-        if(!err) return done('No error!');
-
-        expect(result).to.be.equal(undefined);
-        done();
-      });
-    });
-
-    it('should get today\'s news feed about emergencies', function(done) {
-      gwp.getTodays({lang: 'ka'}, function(err, result) {
-        if(err) return done(err);
-
-        expect(err).to.be.equal(null);
-        expect(result).to.be.a('array');
-        done();
-      });
-    });
+    
 
   });
 
